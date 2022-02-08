1294d842969802cac6cf6d5a631e8075c15cbf02
Enea Pavanello <eneapavanello@gmail.com>
2022-02-08 17:25:32 +0100

    wip

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -1,1 +1,11 @@
-// Modal
+<div class="fixed top-0 inset-x-0 px-4 pt-6 z-50 sm:px-0 sm:flex sm:items-top sm:justify-center ">
+    <div class="fixed inset-0 transform transition-all">
+        <div class="absolute inset-0 bg-blue-900 opacity-30"></div>
+    </div>
+
+    <div class="bg-white rounded-md overflow-hidden shadow-md transform transition-all sm:w-full sm:max-w-3xl">
+        <p>
+            modal
+        </p>
+    </div>
+</div>
