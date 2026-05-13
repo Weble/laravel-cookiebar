dcc87adfcab605f29a05acb371bf12d4d35fc9dc
Matteo Carbone <matteo@weble.it>
2026-05-13 15:10:20 +0200

    standardize translation keys

--- resources/lang/it/consent.php
+++ resources/lang/it/consent.php
@@ -2,27 +2,27 @@
 
 return [
     'required'                  => [
-        'label'         => 'Strettamente necessari',
+        'title'         => 'Strettamente necessari',
         'description'   => 'Questi strumenti di tracciamento sono strettamente necessari per garantire il funzionamento e la fornitura del servizio che ci hai richiesto e, pertanto, non richiedono il tuo consenso.',
     ],
     'ad_storage'                => [
-        'label'         => 'Targeting e Pubblicità',
+        'title'         => 'Targeting e Pubblicità',
         'description'   => 'Questi strumenti di tracciamento ci consentono di fornirti contenuti commerciali personalizzati in base al tuo comportamento e di gestire, fornire e tracciare gli annunci pubblicitari.',
     ],
     'analytics_storage'         => [
-        'label'         => 'Misurazione',
+        'title'         => 'Misurazione',
         'description'   => 'Questi strumenti di tracciamento ci permettono di misurare il traffico e analizzare il tuo comportamento con l\'obiettivo di migliorare il nostro servizio.',
     ],
     'functional_storage'        => [
-        'label'         => 'Funzionali',
+        'title'         => 'Funzionali',
         'description'   => 'Funzionali',
     ],
     'personalization_storage'   => [
-        'label'         => 'Personalizzazione',
+        'title'         => 'Personalizzazione',
         'description'   => 'Personalizzazione',
     ],
     'security_storage'          => [
-        'label'         => 'Sicurezza',
+        'title'         => 'Sicurezza',
         'description'   => 'Sicurezza',
     ],
 ];
